46754b8de1d4eaf0b7f04f386828531f5dd56c00
Andy Butland <abutland@zonedigital.com>
2018-07-25 20:52:55 +0200

    Formatted tags, display result and refresh link

---
+++ BirdRecognizer.Predict/Extensions/StringExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace BirdRecognizer.Predict.Extensions
+{
+    using System.Linq;
+
+    public static class StringExtensions
+    {
+        public static string FirstCharToUpper(this string input)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                return input;
+            }
+
+            return input.First().ToString().ToUpper() + input.Substring(1);
+        }
+    }
+}
--- BirdRecognizer.Predict/Models/DisplayViewModel.cs
+++ BirdRecognizer.Predict/Models/DisplayViewModel.cs
@@ -1,6 +1,7 @@
 ﻿namespace BirdRecognizer.Predict.Models
 {
     using System.Collections.Generic;
+    using System.Linq;
 
     public class DisplayViewModel
     {
@@ -8,7 +9,34 @@
 
         public string ClassificationStatus { get; set; }
 
-        public IList<PredictionDetail> Predictions { get; set; }
+        public string PredictionSummary
+        {
+            get
+            {
+                if (!Predictions.Any())
+                {
+                    return string.Empty;
+                }
+
+                var topPrediction = Predictions
+                    .OrderByDescending(x => x.Probability)
+                    .First();
+
+                if (topPrediction.Probability > 0.95)
+                {
+                    return $"I'm pretty sure that's a {topPrediction.Tag}";
+                }
+
+                if (topPrediction.Probability > 0.85)
+                {
+                    return $"Not sure, but I think it might be a {topPrediction.Tag}";
+                }
+
+                return "Sorry, I can't make that one out.";
+            }
+        }
+
+        public IList<PredictionDetail> Predictions { get; set; } = new List<PredictionDetail>();
 
         public class PredictionDetail
         {
